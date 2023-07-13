a87419241af169ecb87ac0787e8b31604fe01d0f
Sihle <sihle.mtshali8@gmail.com>
2023-07-13 16:25:47 -0700

    04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
-int main(void)
 /* C code that prints "Programming is like building a multilingual puzzle"*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
