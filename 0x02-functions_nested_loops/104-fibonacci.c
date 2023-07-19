5dccec595bc02a932ab4dc7851e4440bf4a1d9f8
Sihle <sihle.mtshali8@gmail.com>
2023-07-18 17:19:05 -0700

    16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,42 +1,59 @@
 #include <stdio.h>
 
+#define SIZE 100
+
+/**
+ * add_arrays - add two big numbers represented as arrays of digits
+ * @a: first number
+ * @b: second number
+ * @res: result
+ */
+void add_arrays(int *a, int *b, int *res)
+{
+int carry = 0;
+for (int i = 0; i < SIZE; i++)
+{
+res[i] = a[i] + b[i] + carry;
+carry = res[i] / 10;
+res[i] %= 10;
+}
+}
+
+/**
+ * print_array - print a big number represented as an array of digits
+ * @a: number to print
+ */
+void print_array(int *a)
+{
+int i = SIZE - 1;
+while (i > 0 && a[i] == 0)
+i--;
+while (i >= 0)
+printf("%d", a[i--]);
+}
+
 /**
  * main - print first 98 Fibonacci numbers
- *
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
 int main(void)
 {
-int count, m;
-unsigned long i = 1, j = 2, t, s;
-unsigned long i_start, j_start, t_start, s_start;
-printf("%lu, %lu", i, j);
-for (count = 3; count <= 92; count++)
+int i;
+int fib1[SIZE] = {0}, fib2[SIZE] = {1}, fib3[SIZE] = {0};
+fib1[0] = 1;
+fib2[0] = 2;
+printf("%d, %d", fib1[0], fib2[0]);
+for (i = 3; i <= 98; i++)
 {
-t = i + j;
-i = j;
-j = t;
-printf(", %lu", j);
+add_arrays(fib1, fib2, fib3);
+print_array(fib3);
+if (i != 98)
+printf(", ");
+for (int j = 0; j < SIZE; j++)
+{
+fib1[j] = fib2[j];
+fib2[j] = fib3[j];
 }
-i_start = i / 1000000000000000;
-j_start = j / 1000000000000000;
-i %= 1000000000000000;
-j %= 1000000000000000;
-for (; count <= 98; count++)
-{
-t_start = i_start;
-t = i;
-s_start = j_start + i_start + ((i + j) / 1000000000000000);
-s = (i + j) % 1000000000000000;
-printf(", %lu", s_start);
-m = 16 - (s > 0 ? (int)log10((double)s) : 0);
-while (m--)
-printf("0");
-printf("%lu", s);
-i_start = j_start;
-i = j;
-j_start = s_start;
-j = s;
 }
 printf("\n");
 return (0);
