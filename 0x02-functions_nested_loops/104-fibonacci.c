f4a5222b604fb611d3b14510620144b7fef99531
Sihle <sihle.mtshali8@gmail.com>
2023-07-18 17:52:50 -0700

    16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,60 +1,48 @@
-#include <stdio.h>
-
-#define SIZE 100
+#include<stdio.h>
 
 /**
- * add_arrays - add two big numbers represented as arrays of digits
- * @a: first number
- * @b: second number
- * @res: result
+ * main - fibonacci
+ *
+ * Purpose - hard fibonacci
+ *
+ * Return:  (Success)
  */
-void add_arrays(int *a, int *b, int *res)
-{
-int i, carry = 0;
-for (i = 0; i < SIZE; i++)
-{
-res[i] = a[i] + b[i] + carry;
-carry = res[i] / 10;
-res[i] %= 10;
-}
-}
 
-/**
- * print_array - print a big number represented as an array of digits
- * @a: number to print
- */
-void print_array(int *a)
-{
-int i = SIZE - 1;
-while (i > 0 && a[i] == 0)
-i--;
-while (i >= 0)
-printf("%d", a[i--]);
-}
-
-/**
- * main - print first 98 Fibonacci numbers
- * Return: Always 0
- */
 int main(void)
 {
-int i, j;
-int fib1[SIZE] = {1}, fib2[SIZE] = {2}, fib3[SIZE] = {0};
-print_array(fib1);
-printf(", ");
-print_array(fib2);
-for (i = 3; i <= 98; i++)
+unsigned long int counter;
+unsigned long int previous = 1;
+unsigned long int current = 2;
+unsigned long int largeNum = 1000000000;
+unsigned long int prevHigh;
+unsigned long int prevLow;
+unsigned long int currHigh;
+unsigned long int currLow;
+
+printf("%lu", previous);
+
+for (counter = 1; counter < 91; counter++)
 {
-add_arrays(fib1, fib2, fib3);
-if (i != 98)
-printf(", ");
-print_array(fib3);
-for (j = 0; j < SIZE; j++)
+printf(", %lu", current);
+current += previous;
+previous = current - previous;
+}
+
+prevHigh = (previous / largeNum);
+prevLow = (previous % largeNum);
+currHigh = (current / largeNum);
+currLow = (current % largeNum);
+
+for (counter = 92; counter < 99; counter++)
 {
-fib1[j] = fib2[j];
-fib2[j] = fib3[j];
+printf(", %lu", currHigh + (currLow / largeNum));
+printf("%lu", currLow % largeNum);
+currHigh = currHigh + prevHigh;
+prevHigh = currHigh - prevHigh;
+currLow = currLow + prevLow;
+prevLow = currLow - prevLow;
 }
-}
+
 printf("\n");
 return (0);
 }
