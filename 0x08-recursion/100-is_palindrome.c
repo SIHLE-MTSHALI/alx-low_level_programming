177547f3c9a641953cd27310a0322058c575eb13
Sihle <sihle.mtshali8@gmail.com>
2023-08-02 12:53:49 -0700

    07

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,28 +1,32 @@
 #include "main.h"
 
 /**
+ * find_length - finds the length of a string
+ * @s: the string
+ * @len: the current length
+ * Return: the length of the string
+ */
+int find_length(char *s, int len)
+{
+if (s[len] == '\0')
+return (len);
+return (find_length(s, len + 1));
+}
+
+/**
  * check_palindrome - checks if a string is a palindrome
  * @s: the string to check
  * @start: the starting index
  * @end: the ending index
- * @len: the length of the string
  * Return: 1 if a string is a palindrome, and 0 if not
  */
-int check_palindrome(char *s, int start, int end, int len)
-{
-if (len == 0)
+int check_palindrome(char *s, int start, int end)
 {
 if (start >= end)
-{
 return (1);
-}
 if (s[start] != s[end])
-{
 return (0);
-}
-return (check_palindrome(s, start + 1, end - 1, len));
-}
-return (check_palindrome(s, start + 1, end, len + 1));
+return (check_palindrome(s, start + 1, end - 1));
 }
 
 /**
@@ -32,11 +36,7 @@ return (check_palindrome(s, start + 1, end, len + 1));
  */
 int is_palindrome(char *s)
 {
-int len = 0;
-while (s[len] != '\0')
-{
-len++;
-}
-return (check_palindrome(s, 0, len - 1, 0));
+int len = find_length(s, 0);
+return (check_palindrome(s, 0, len - 1));
 }
 
