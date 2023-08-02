9b3a790229f0ac5022d77a64095ecbeeb3362484
Sihle <sihle.mtshali8@gmail.com>
2023-08-01 18:25:50 -0700

    02

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_strchr - Locates a character in a string
+*@s: The string to be searched
+*@c: The character to search for
+*
+*Return: A pointer to the first occurrence of the character or NULL
+*/
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return ((char *)0);
+}
+
