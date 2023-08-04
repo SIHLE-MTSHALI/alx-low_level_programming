00e7031b819ae0a3b6521d96a68759906c899f33
Sihle <sihle.mtshali8@gmail.com>
2023-08-04 14:32:14 -0700

    00

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase
+ * @c: Character to check
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+(void)c; /* This will silence the warning */
+return (0);
+}
+
