013df3385ed1f1eab2b2dcfe4d5b86d40706eaed
Sihle <sihle.mtshali8@gmail.com>
2023-08-10 11:53:14 -0700

    05 again

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,8 +1,9 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * error_exit - exits with status 98 and prints Error
+ * error_exit - Prints "Error" and exits with status 98
  */
 void error_exit(void)
 {
@@ -11,66 +12,43 @@ _putchar('r');
 _putchar('r');
 _putchar('o');
 _putchar('r');
+_putchar('\n');
 exit(98);
 }
 
 /**
- * is_digit - checks if a string only contains digits
- * @s: string to check
+ * is_digit - Check if a string has only digits
+ * @s: String to check
  *
- * Return: 1 if string only contains digits, 0 otherwise
+ * Return: 1 if string has only digits, otherwise 0
  */
 int is_digit(char *s)
 {
-int i = 0;
-while (s[i])
+while (*s)
 {
-if (s[i] < '0' || s[i] > '9')
+if (*s < '0' || *s > '9')
 return (0);
-i++;
+s++;
 }
 return (1);
 }
 
 /**
- * main - multiplies two positive numbers and prints the result
- * @argc: number of arguments passed to the program
- * @argv: array of arguments
+ * main - Entry point, multiplies two numbers
+ * @argc: Argument count
+ * @argv: Array of argument strings
  *
- * Return: 0 on success, 98 on error
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
 {
-int i, j, carry, len1, len2, total_len;
-int res[10000] = {0};
-
+unsigned long num1, num2, result;
 if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 error_exit();
-
-len1 = strlen(argv[1]);
-len2 = strlen(argv[2]);
-total_len = len1 + len2;
-
-for (i = len1 - 1; i >= 0; i--)
-{
-carry = 0;
-for (j = len2 - 1; j >= 0; j--)
-{
-int sum = (argv[1][i] - '0') * (argv[2][j] - '0') + res[i + j + 1] + carry;
-res[i + j + 1] = sum % 10;
-carry = sum / 10;
-}
-res[i + j + 1] = carry;
-}
-
-for (i = 0; i < total_len; i++)
-{
-if (i == 0 && res[i] == 0)
-continue;
-_putchar(res[i] + '0');
-}
-_putchar('\n');
-
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 *num2;
+printf("%lu\n", result);
 return (0);
 }
 
