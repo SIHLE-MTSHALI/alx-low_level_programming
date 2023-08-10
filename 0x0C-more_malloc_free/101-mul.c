974821bcd29d02bbc7d31c4006e38b71f54daf7e
Sihle <sihle.mtshali8@gmail.com>
2023-08-10 13:22:01 -0700

    05 oh no!

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,27 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+
+#define ERROR_MESSAGE "Error"
 
 /**
-* error_exit - Exit with custom error message.
+* validate_string_as_number - checks if string is a number
+* @str: string to check
+*
+* Return: 1 if string is a number, 0 otherwise
 */
-void error_exit(void)
-{
-_putchar('E');
-_putchar('r');
-_putchar('o');
-_putchar('r');
-_putchar('\n');
-exit(98);
-}
-
-/**
-* is_digit - Check if a string consists only of digits.
-* @str: The string to check.
-* Return: 1 if string is made of digits, 0 otherwise.
-*/
-int is_digit(char *str)
+int validate_string_as_number(char *str)
 {
 while (*str)
 {
@@ -33,47 +22,105 @@ return (1);
 }
 
 /**
-* main - Multiplies two positive numbers.
-* @argc: Argument count.
-* @argv: Argument vector.
-* Return: 0 on success, 98 on error.
+* string_length - finds the length of a string
+* @str: string to check
+*
+* Return: length of the string
 */
-int main(int argc, char *argv[])
+int string_length(char *str)
 {
-char *num1, *num2, *result;
-int len1, len2, lenr, i, j, carry, temp;
+int len = 0;
+while (*str++)
+len++;
+return (len);
+}
 
-if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-error_exit();
-
-num1 = argv[1];
-num2 = argv[2];
-len1 = strlen(num1);
-len2 = strlen(num2);
-lenr = len1 + len2 + 1;
-result = calloc(lenr, sizeof(char));
-if (!result)
-error_exit();
-
-for (i = len1 - 1; i >= 0; i--)
+/**
+* display_error_and_exit - prints error message and exits
+*/
+void display_error_and_exit(void)
 {
+printf("%s\n", ERROR_MESSAGE);
+exit(98);
+}
+
+/**
+* perform_multiplication - handles the core multiplication logic
+* @num1: first number as a string
+* @num2: second number as a string
+* @len_num1: length of num1
+* @len_num2: length of num2
+*
+* Return: pointer to the product array
+*/
+int *perform_multiplication(char *num1, char *num2, int len_num1, int len_num2)
+{
+int *product, idx, carry, digit_num1, digit_num2, total_length;
+
+total_length = len_num1 + len_num2 + 1;
+product = malloc(sizeof(int) * total_length);
+if (!product)
+return (NULL);
+for (idx = 0; idx <= len_num1 + len_num2; idx++)
+product[idx] = 0;
+
+for (; len_num1 >= 0; len_num1--)
+{
+digit_num1 = num1[len_num1] - '0';
 carry = 0;
-for (j = len2 - 1; j >= 0; j--)
+for (len_num2 = string_length(num2) - 1; len_num2 >= 0; len_num2--)
 {
-temp = (num1[i] - '0') * (num2[j] - '0') + carry + (result[i + j + 1] - '0');
-result[i + j + 1] = (temp % 10) + '0';
-carry = temp / 10;
+digit_num2 = num2[len_num2] - '0';
+carry += product[len_num1 + len_num2 + 1] + (digit_num1 *digit_num2);
+product[len_num1 + len_num2 + 1] = carry % 10;
+carry /= 10;
 }
-result[i + j + 1] = carry + '0';
+if (carry > 0)
+product[len_num1 + len_num2 + 1] += carry;
+}
+return (product);
 }
 
-for (i = 0; result[i] == '0' && i < lenr - 1; i++)
-;
-while (i < lenr)
-_putchar(result[i++]);
+/**
+* main - multiplies two positive numbers
+* @arg_count: number of arguments
+* @args: array of arguments
+*
+* Return: 0 on success, 1 on failure
+*/
+int main(int arg_count, char **args)
+{
+char *num1, *num2;
+int len_num1, len_num2, total_length, idx, flag = 0;
+int *product;
+
+num1 = args[1];
+num2 = args[2];
+
+if (arg_count != 3 || !validate_string_as_number(num1) ||
+!validate_string_as_number(num2))
+display_error_and_exit();
+
+len_num1 = string_length(num1);
+len_num2 = string_length(num2);
+total_length = len_num1 + len_num2 + 1;
+
+product = perform_multiplication(num1, num2, len_num1, len_num2);
+if (!product)
+return (1);
+
+for (idx = 0; idx < total_length - 1; idx++)
+{
+if (product[idx])
+flag = 1;
+if (flag)
+_putchar(product[idx] + '0');
+}
+if (!flag)
+_putchar('0');
 _putchar('\n');
 
-free(result);
+free(product);
 return (0);
 }
 
