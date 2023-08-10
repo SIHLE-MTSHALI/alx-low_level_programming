32614abb8fdd01d778db12327b00866c56e3b7ce
Sihle <sihle.mtshali8@gmail.com>
2023-08-10 11:37:31 -0700

    05 edited

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,48 +1,76 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 /**
-* error_exit - exits with a status of 98 and prints Error
-*/
+ * error_exit - exits with status 98 and prints Error
+ */
 void error_exit(void)
 {
-printf("Error\n");
+_putchar('E');
+_putchar('r');
+_putchar('r');
+_putchar('o');
+_putchar('r');
 exit(98);
 }
 
 /**
-* is_digit - checks if a string contains only digits
-* @s: string to check
-* Return: 1 if s contains only digits, 0 otherwise
-*/
+ * is_digit - checks if a string only contains digits
+ * @s: string to check
+ *
+ * Return: 1 if string only contains digits, 0 otherwise
+ */
 int is_digit(char *s)
 {
-if (!*s)
-return (0);
-
-while (*s)
+int i = 0;
+while (s[i])
 {
-if (*s < '0' || *s > '9')
+if (s[i] < '0' || s[i] > '9')
 return (0);
-s++;
+i++;
 }
-
 return (1);
 }
 
 /**
-* main - multiplies two numbers and prints the result
-* @argc: number of arguments
-* @argv: array of argument strings
-* Return: Always 0
-*/
+ * main - multiplies two positive numbers and prints the result
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 98 on error
+ */
 int main(int argc, char *argv[])
 {
+int i, j, carry, len1, len2, total_len;
+int res[10000] = {0};
+
 if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 error_exit();
 
-printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+len1 = strlen(argv[1]);
+len2 = strlen(argv[2]);
+total_len = len1 + len2;
+
+for (i = len1 - 1; i >= 0; i--)
+{
+carry = 0;
+for (j = len2 - 1; j >= 0; j--)
+{
+int sum = (argv[1][i] - '0') * (argv[2][j] - '0') + res[i + j + 1] + carry;
+res[i + j + 1] = sum % 10;
+carry = sum / 10;
+}
+res[i + j + 1] = carry;
+}
+
+for (i = 0; i < total_len; i++)
+{
+if (i == 0 && res[i] == 0)
+continue;
+_putchar(res[i] + '0');
+}
+_putchar('\n');
+
 return (0);
 }
 
