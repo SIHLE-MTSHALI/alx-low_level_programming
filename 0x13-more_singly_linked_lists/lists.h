59c67e558d2c7c5b0d1420a79434ee6299f0e82e
Sihle <sihle.mtshali8@gmail.com>
2023-08-28 15:24:26 -0700

    01

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,11 @@ struct listint_s *next;
 
 /* Function Prototypes */
 size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+
+
+
 
 #endif /* LISTS_H */
 
