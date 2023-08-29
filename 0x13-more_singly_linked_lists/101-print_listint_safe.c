94e60219d7683b70c648307b2989b2b33a40a776
Sihle <sihle.mtshali8@gmail.com>
2023-08-29 14:07:27 -0700

    12 fixed again

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * print_listint_safe - prints a listint_t linked list, even if it has a loop
@@ -10,7 +11,10 @@ size_t print_listint_safe(const listint_t *head)
 {
 const listint_t *temp = head;
 size_t count = 0;
-
+if (head == NULL)
+{
+exit(98);
+}
 while (temp)
 {
 printf("[%p] %d\n", (void *)temp, temp->n);
@@ -22,7 +26,6 @@ break;
 }
 temp = temp->next;
 }
-
 return (count);
 }
 
