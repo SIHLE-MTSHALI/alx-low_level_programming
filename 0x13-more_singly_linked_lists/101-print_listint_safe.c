cffcd0e1d155022b201e2d0b089cccc2fc3d4454
Sihle <sihle.mtshali8@gmail.com>
2023-08-29 15:24:33 -0700

    12 Let us see again

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,35 +11,21 @@ size_t print_listint_safe(const listint_t *head)
 {
 const listint_t *fast = head, *slow = head;
 size_t count = 0;
-int loop_detected = 0;
 
 if (head == NULL)
-exit(98);
+return (0);
 
-/* Use Floyd's cycle-finding algorithm to detect a loop */
 while (fast && fast->next && slow)
 {
+printf("[%p] %d\n", (void *)slow, slow->n);
+count++;
 fast = fast->next->next;
 slow = slow->next;
+
 if (fast == slow)
 {
-loop_detected = 1;
-break;
-}
-}
-
-/* Print the list */
-while (head)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-count++;
-
-if (loop_detected)
-{
-fast = fast->next;
-if (fast == head)
-break;
+printf("-> [%p] %d\n", (void *)slow, slow->n);
+exit(98);
 }
 }
 
