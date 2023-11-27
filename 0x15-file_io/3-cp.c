01d876c0d244856f68cef89636897d40ad2911f4
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2023-11-27 12:38:14 -0800

    03 fixed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
@@ -45,8 +45,8 @@ int main(int argc, char *argv[])
 
 	if (read_status == -1)
 	{
-		close(file_from), close(file_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	close(file_from), close(file_to);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 
 	close_file(file_from);
