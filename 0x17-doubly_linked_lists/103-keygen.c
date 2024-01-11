6600b7577e90cae10a0b6c305a38e45e02326526
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-01-11 15:08:29 -0800

    11 fix

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,50 +1,52 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
- * generate_key - Generates a key for a given username based on the
- *                crackme5 algorithm.
- * @username: The username for which to generate the key.
+ * main - Generate a key based on a username for crackme5
+ * @argc: Number of arguments passed
+ * @argv: Arguments passed to main
  *
- * Return: The generated key.
- */
-int generate_key(const char *username)
-{
-	int length = strlen(username);
-	int key = ((length ^ 59) & 63) + 32;
-	int i;
-
-	for (i = 0; i < length; i++)
-	{
-		if (username[i] % 2 == 0)
-		{
-			key += 1;
-		}
-	}
-	key = ((key ^ 0x4f) & 63) + 32;
-	return (key);
-}
-
-/**
- * main - Entry point for the keygen program.
- * @argc: Argument count.
- * @argv: Argument vector.
- *
- * Return: 0 on success, 1 on error.
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	int key;
+	unsigned int idx, sum, prod, max_char;
+	size_t length, square_sum;
+	char *key_chars = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char key[7] = "      ";
 
 	if (argc != 2)
 	{
-		printf("Usage: ./keygen5 username\n");
+		printf("Correct usage: ./keygen username\n");
 		return (1);
 	}
+	length = strlen(argv[1]);
+	key[0] = key_chars[(length ^ 59) & 63];
 
-	key = generate_key(argv[1]);
-	printf("%d\n", key);
+	for (idx = 0, sum = 0; idx < length; idx++)
+		sum += argv[1][idx];
+	key[1] = key_chars[(sum ^ 79) & 63];
 
+	for (idx = 0, prod = 1; idx < length; idx++)
+		prod *= argv[1][idx];
+	key[2] = key_chars[(prod ^ 85) & 63];
+
+	for (max_char = argv[1][0], idx = 0; idx < length; idx++)
+		if ((char)max_char <= argv[1][idx])
+			max_char = argv[1][idx];
+	srand(max_char ^ 14);
+	key[3] = key_chars[rand() & 63];
+
+	for (square_sum = 0, idx = 0; idx < length; idx++)
+		square_sum += argv[1][idx] * argv[1][idx];
+	key[4] = key_chars[(square_sum ^ 239) & 63];
+
+	for (sum = 0, idx = 0; (char)idx < argv[1][0]; idx++)
+		sum = rand();
+	key[5] = key_chars[(sum ^ 229) & 63];
+
+	printf("%s\n", key);
 	return (0);
 }
 
