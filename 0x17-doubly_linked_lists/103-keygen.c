cb7ad105ad2d4c9f757bcb8cf2cd2073482edf48
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-01-11 14:41:14 -0800

    11++++

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 
-int generate_key(const char *username) {
-    int length = strlen(username);
-    int key = ((length ^ 59) & 63) + 32;
-    for (int i = 0; i < length; i++) {
-        if (username[i] % 2 == 0) {
-            key += 1;
-        }
-    }
-    key = ((key ^ 0x4f) & 63) + 32;
-    return key;
+/**
+ * generate_key - Generates a key for a given username based on the
+ *                crackme5 algorithm.
+ * @username: The username for which to generate the key.
+ *
+ * Return: The generated key.
+ */
+int generate_key(const char *username)
+{
+	int length = strlen(username);
+	int key = ((length ^ 59) & 63) + 32;
+	int i;
+
+	for (i = 0; i < length; i++)
+	{
+		if (username[i] % 2 == 0)
+		{
+			key += 1;
+		}
+	}
+	key = ((key ^ 0x4f) & 63) + 32;
+	return (key);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: ./keygen5 username\n");
-        return 1;
-    }
-    int key = generate_key(argv[1]);
-    printf("%d\n", key);
-    return 0;
+/**
+ * main - Entry point for the keygen program.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(int argc, char *argv[])
+{
+	int key;
+
+	if (argc != 2)
+	{
+		printf("Usage: ./keygen5 username\n");
+		return (1);
+	}
+
+	key = generate_key(argv[1]);
+	printf("%d\n", key);
+
+	return (0);
 }
 
