3deeda8afbe09821c4a630e04e38e9658cdfcc0e
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-05-08 18:26:09 -0700

    Implement jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,27 +10,25 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t step = sqrt(size), prev = 0, i;
+	size_t step = sqrt(size), prev = 0, i;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (prev < size && array[prev] < value)
-    {
-        printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
-        prev += step;
-    }
+	while (prev < size && array[prev] < value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
+		prev += step;
+	}
 
-    printf("Value found between indexes [%ld] and [%ld]\n", prev - step,
-           prev < size ? prev : size - 1);
+	printf("Value found between indexes [%ld] and [%ld]\n",
+		prev - step, prev < size ? prev : size - 1);
 
-    for (i = prev - step; i < size && array[i] < value; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-    }
+	for (i = prev - step; i < size && array[i] < value; i++)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
-    if (i < size && array[i] == value)
-        return (i);
+	if (i < size && array[i] == value)
+		return (i);
 
-    return (-1);
+	return (-1);
 }
