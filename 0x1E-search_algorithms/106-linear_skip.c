96e38ef9b75a4eedf821f140da53e1d8e8fa6858
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-05-08 18:58:46 -0700

    Implement linear search in a skip list

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -5,7 +5,7 @@
  * @list: Pointer to the head of the skip list
  * @value: Value to search for
  *
- * Return: Pointer to the first node where value is located
+ * Return: Pointer to the first node where value is located,
  *         or NULL if not found
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
@@ -15,30 +15,26 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	if (list == NULL)
 		return (NULL);
 
-	while (curr->express != NULL && curr->n < value)
+	while (curr->express && curr->n < value)
 	{
 		prev = curr;
 		curr = curr->express;
 		printf("Value checked at index [%ld] = [%d]\n",
-			curr->index, curr->n);
+		       curr->index, curr->n);
 	}
 
 	printf("Value found between indexes [%ld] and [%ld]\n",
-		prev->index, curr->index);
+	       prev->index, curr->index);
 
-	while (prev != curr)
+	while (prev && prev->n < value)
 	{
 		printf("Value checked at index [%ld] = [%d]\n",
-			prev->index, prev->n);
-		if (prev->n == value)
-			return (prev);
+		       prev->index, prev->n);
 		prev = prev->next;
 	}
 
-	printf("Value checked at index [%ld] = [%d]\n",
-		curr->index, curr->n);
-	if (curr->n == value)
-		return (curr);
+	if (prev && prev->n == value)
+		return (prev);
 
 	return (NULL);
 }
